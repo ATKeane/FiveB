cdba4a549343b6579c8be1716a563ee1f651d2d3
ATKeane <56166739+ATKeane@users.noreply.github.com>
2023-05-11 11:41:07 -0400

    Update BestGame.py

--- BestGame.py
+++ BestGame.py
@@ -1,33 +1,45 @@
 #Code for determining the best game -
 
 import pandas as pd
+import os.path
+from percentage_stats import total_bases
 
-'''
-def StatsDFPrepare(csv):
-    GameStats = pd.read_csv(csv)
-    return GameStats
-'''
-#week = input("What Week?")
-#Temporary putting this in to speed up testing.
-week = 2
-Game1Stats = pd.read_csv("Week"+str(week)+"Game1.csv")
-Game2Stats = pd.read_csv("Week"+str(week)+"Game2.csv")
+week = input("What Week?")
+
+
+#Fix This - Needless repeating.
+path = ("Week" + week + " Files/")
+path1 = ("Week" + week + " Files/" + "Game1/")
+path2 = ("Week" + week + " Files/" + "Game2/")
+
+Game1Stats = pd.read_csv(os.path.join(path1, "Week"+str(week)+"Game1.csv"))
+Game2Stats = pd.read_csv(os.path.join(path2, "Week"+str(week)+"Game2.csv"))
 
 #Adding the Weeks together
 FullStats = Game1Stats.append(Game2Stats)
 
-#Empty Dataframe to put the best game for each player into
-BestGame = pd.DataFrame()
+#Total Bases on FullStats
+FullStats = total_bases(FullStats)
 
-#Unique Index items - here each individual player.
-players=FullStats.Name.unique()
+#Going through the ranking list, in order, to determine best game
 
-for player in players:
-    playerStats=FullStats[FullStats['Name']==player]
-    BestGamePlayer = (playerStats[playerStats.OPS == playerStats.OPS.max()])
-    BestGame = BestGame.append(BestGamePlayer)
+def best_game(gamebook, ranking_list):
+    BestGame = pd.DataFrame()
+    players=gamebook.Name.unique()
+    for player in players:
+        playerStats=gamebook[gamebook['Name']==player]
+        for stat in ranking_list:
+            BestGamePlayer = (playerStats[playerStats[stat]==playerStats[stat].max()])
+            if len(BestGamePlayer.index) == 1:
+                BestGame = BestGame.append(BestGamePlayer)
+                break
+    return(BestGame)
+
+ranking_list = ['OPS','Home Run','Total Bases','RBI','R','Win','PA']
+
+BestGame =best_game(FullStats, ranking_list)
 
 #Sorting by name
 BestGame = BestGame.sort_values(by=['Name'])
 
-BestGame.to_csv("BestGame.csv")
+BestGame.to_csv(os.path.join(path, "BestGame.csv"))
