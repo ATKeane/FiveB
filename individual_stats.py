3db60c30fc93b7422e2348a66a921b285ff3622c
ATKeane <56166739+ATKeane@users.noreply.github.com>
2023-05-11 11:44:58 -0400

    Update individual_stats.py
    Path to Folder

--- individual_stats.py
+++ individual_stats.py
@@ -3,9 +3,8 @@
 import pandas as pd
 import os.path
 
-#Commenting this out for now - fix for actual use. 
-#total_weeks = input("How many weeks so far?")
-total_weeks = 2
+
+total_weeks = input("How many weeks so far?")
 counter = 1
 weeks = []
 games = ["Game1","Game2"]
@@ -15,8 +14,8 @@ while counter <= int(total_weeks):
 
 
 #Building a list of players
-SeasonTotals = pd.read_csv('SeasonFiles/SeasonTotals.csv')
-players = SeasonTotals['Name']
+player_names = pd.read_csv("PlayerList.csv")
+players = list(player_names["Player"])
 
 #Creating an empty data frame - this will have each game appended onto it.
 player_totals = pd.DataFrame()
@@ -25,13 +24,17 @@ player_totals = pd.DataFrame()
 #IMPROVEMENT NOTE: Move to a function for the week_location?
 for week in weeks:
     for game in games:
-        week_location = week + " Files/" + game + "/" + week + game + ".csv"
-        week_game = pd.read_csv(week_location)
+        week_path = week + " Files/" + game + "/"
+        week_file = week+game+".csv"
+        week_game = pd.read_csv(week_path+week_file)
         player_totals = player_totals.append(week_game)
 
 print(player_totals)
-    
+   
 for player in players:
     player_sheet = player_totals[player_totals['Name']==player]
     file_name = player + ".csv"
-    player_sheet.to_csv(os.path.join('IndividualStats', file_name))
+    print(file_name)
+    print(player_sheet)
+    player_sheet.to_csv(os.path.join('Individual Stats/', file_name))
+
